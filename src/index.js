2d05f57404bdbe5cfa8619032269e043e1549624
Muhammad Zawar <mzawar189@gmail.com>
2024-08-02 22:02:31 +0500

    Bytewise Task 8

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import ImageGen2 from "./imageG2";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <ImageGen2 />
+  </React.StrictMode>
+);
